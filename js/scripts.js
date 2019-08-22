8a78d674e21888d5cb23b86aa31e6159b6f5b296
Anita Kemp and Christine Frank <anita@>
2019-08-22 15:05:25 -0700

    Add comments to explain logic

--- js/scripts.js
+++ js/scripts.js
@@ -34,25 +34,27 @@ function convertNumber(numberInput) {
     } else {
       for (var count = 0; count < numberArray.length; count++) {
         var symbolValue = numberArray[count][0];
-        if (numberInput - symbolValue >= 0) {
-          while (numberInput - symbolValue >= 0) {
+        if (numberInput - symbolValue >= 0) {  // check if symbol should be used immediately
+          while (numberInput - symbolValue >= 0) { // allow repeating symbols
             output += numberArray[count][1];
             numberInput -= symbolValue;
           }
         }
-        if (count < 6) {
+        if (count < 6) { // enables subtraction notations, not needed for I numeral
           var newSymbol = numberArray[count + 1][1];
           var nextSymbolValue = numberArray[count + 1][0];
-          if (nextSymbolValue.toString()[0] === "5") {
+          if (nextSymbolValue.toString()[0] === "5") { // skip 5--- numerals
             newSymbol = numberArray[count + 2][1];
             nextSymbolValue = numberArray[count + 2][0];
           }
           if (numberInput / nextSymbolValue > 3 && symbolValue - numberInput <= nextSymbolValue) {
+            // trigger subtraction notation when more than 3 symbols are needed
             numberInput = numberInput - (symbolValue - nextSymbolValue);
             output += newSymbol + numberArray[count][1];
           }
         }
         if ((numberInput < 10 && numberInput > 0) && symbolValue - 1 === numberInput) {
+          // trigger special case subtraction notation for single digits
           numberInput = numberInput - symbolValue;
           output += "I" + numberArray[count][1];
         }
