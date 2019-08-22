fe62d061dd6d335eaedb2215d62f9a81d235f59c
Anita Kemp and Christine Frank <christine@>
2019-08-22 10:12:17 -0700

    Add the sum of the symbol

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,26 @@ $(document).ready(function(){
 function convertNumber (numberInput){
   var numberArray = [[1, 'I'], [5, 'V'], [10, 'X'], [50, 'L'], [100, 'C'], [500,
   'D'], [1000, 'M']];
-  var numberMap = new Map(numberArray);
-
+  var numberMap = new Map(numberArray.reverse());
+  var output = '';
   if (numberInput > 3999){
     alert("Please enter a number less than 3,999");
   } else {
     if (numberMap.get(numberInput)){
       return numberMap.get(numberInput);
     } else {
-      return numberInput ;
+      for (var i=numberInput; i>0; i){
+        for (var mapItem of numberMap.entries()){
+          var symbolValue = mapItem[0];
+          while (numberInput - symbolValue >=0){
+            output += mapItem[1];
+            numberInput -= symbolValue;
+
+          }
+        }
+        i=0;
+      }
+      return output ;
     }
   }
 
